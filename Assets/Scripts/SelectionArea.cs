3c43f90c4aec77fa5d4c269956254b34f59dd0d2
nourgaser <nourgaser2012@gmail.com>
2023-01-12 15:07:37 +0200

    Finished selection

--- Assets/Scripts/SelectionArea.cs
+++ Assets/Scripts/SelectionArea.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 
 public class SelectionArea : MonoBehaviour
 {
-    private SpriteRenderer sprite;
 
-    private void Start()
-    {
-        sprite = GetComponentInChildren<SpriteRenderer>();
-    }
     public void Enable()
     {
         gameObject.SetActive(true);
@@ -27,16 +22,14 @@ public class SelectionArea : MonoBehaviour
 
     public void Adjust(Vector2 currentPosition)
     {
-        // transform.position -= sprite.bounds.extents;
         transform.localScale = currentPosition - (Vector2)transform.position;
-        // transform.position += sprite.bounds.extents;
     }
 
     public List<ComponentBehavior> GetSelection()
     {
         List<ComponentBehavior> matches = new List<ComponentBehavior>();
 
-        Collider2D[] collidedObjectsArray = Physics2D.OverlapAreaAll(transform.position, transform.localScale);
+        Collider2D[] collidedObjectsArray = Physics2D.OverlapAreaAll(transform.position, transform.position + transform.localScale);
         foreach (var collidedObject in collidedObjectsArray)
         {
             ComponentBehavior componentObject = collidedObject.GetComponent<ComponentBehavior>();
