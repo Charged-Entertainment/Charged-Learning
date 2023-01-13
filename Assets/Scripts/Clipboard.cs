1b5305e599f5fdc5a00fc5c19b1c453724787ddd
nourgaser <nourgaser2012@gmail.com>
2023-01-13 09:13:34 +0200

    addressed issue #12

--- Assets/Scripts/Clipboard.cs
+++ Assets/Scripts/Clipboard.cs
@@ -4,39 +4,42 @@ using UnityEngine;
 
 public class Clipboard : MonoBehaviour
 {
-    public List<ComponentBehavior> clipboard { get; private set; }
-    private void Awake()
-    {
-        clipboard = new List<ComponentBehavior>();
-    }
-
     public void Copy(ComponentBehavior[] components, bool isCut = false)
     {
         Debug.Log("Copying!");
-        clipboard.Clear();
+        Clear();
 
         foreach (var component in components)
         {
             ComponentBehavior copy = Instantiate(component, transform);
             copy.gameObject.SetActive(false);
 
-            clipboard.Add(copy);
             if (isCut) GameObject.Destroy(component);
         }
     }
 
-    public void Paste(Vector3 pos)
+    public void Paste(Vector2 pos)
     {
         Debug.Log($"Pasting at {pos}");
         transform.position = pos;
 
-        foreach (var component in clipboard)
+        foreach (var component in GetContent())
         {
             component.gameObject.SetActive(true);
             component.gameObject.transform.parent = null;
-            component.gameObject.transform.position = new Vector3(component.gameObject.transform.position.x, component.gameObject.transform.position.y, 0); 
         }
 
         transform.position = Vector3.zero;
+    }
+
+    public ComponentBehavior[] GetContent() {
+        return GetComponentsInChildren<ComponentBehavior>(true);
+    }
+
+    public void Clear() {
+        foreach (var component in GetContent())
+        {
+            component.Destroy();
+        }
     }
 }
