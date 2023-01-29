25a60cfd2839f2931aa6e4455f6f557c1211598c
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-01-29 21:35:18 +0200

    RichText utilites implemented

---
+++ Assets/Scripts/Ui/RichText.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+/// <summary>Alternative to enums that represents a color by its hex code</summary>
+public class PaletteColor{
+    public string Color {get; private set;}
+    public PaletteColor(string color){Color = color;}
+    public static PaletteColor Red {get {return new PaletteColor("#FF0000");}}
+    public static PaletteColor Green {get {return new PaletteColor("#00FF00");}}
+
+    public override string ToString()
+    {
+        return Color;
+    }
+    //...
+}
+
+public class RichText
+{
+    public string HTML {get; private set;}
+    /// <summary>Uses Regex to parse the text</summary>
+    public string Text{get; private set;}
+
+    RichText(string text){
+        Text = Regex.Replace(text, "<.*?>", string.Empty);
+        HTML = text;
+    }
+    
+    public override string ToString(){
+        return HTML;
+    }
+
+    public static implicit operator string(RichText t){
+        return t.HTML;
+    }
+
+    public static RichText Italic<T>(T text){
+        return new RichText($"<i>{text}</i>");
+    }
+
+    public static RichText Color<T>(T text, PaletteColor color){
+        return new RichText($"<color={color}>{text}</color>");
+    }
+
+    public static RichText Color<T>(T text, string color){
+        return new RichText($"<color={color}>{text}</color>");
+    }
+
+    public static RichText Bold<T>(T text){
+        return new RichText($"<b>{text}</b>");
+    }
+    //... more tags can be added
+
+
+}
--- Assets/Scripts/Ui/UiTerminal.cs
+++ Assets/Scripts/Ui/UiTerminal.cs
@@ -13,64 +13,78 @@ enum WindowState
 
 public class UiTerminal : MonoBehaviour
 {
-    [SerializeField]private List<string> logs;
-    [SerializeField]private List<string> testGoals;
+    [SerializeField] private List<string> logs;
+    [SerializeField] private List<string> testGoals;
     private VisualElement goalsVisualElement;
     private VisualElement logsVisualElement;
     private VisualElement bodyVisualElement;
     private VisualElement terminalWindow;
+    private bool mouseDown = false;
+    private Vector2 dragStartPos;
 
     private WindowState windowState;
     [SerializeField] private bool drawLogs;
 
     void Start()
     {
-        logs = new List<string>{"log1", "log2"};
+        logs = new List<string> { "log1", "log2" };
+        testGoals = new List<string> { "measure whatever in whatever and so whatever", "measure whatever in whatever", "measure whatever in whatever" };
+
         windowState = WindowState.Normal;
         drawLogs = false;
+        mouseDown = false;
+
     }
 
     void OnEnable()
     {
         var rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
-        goalsVisualElement = rootVisualElement.Q("terminal-instance").Q("goals");
-        logsVisualElement = rootVisualElement.Q("terminal-instance").Q("logs");
-        bodyVisualElement = rootVisualElement.Q("terminal-instance").Q("body");
-        terminalWindow = rootVisualElement.Q("terminal-instance").Q("window");
+        var terminalInstance = rootVisualElement.Q("terminal-instance");
+
+        goalsVisualElement = terminalInstance.Q("goals");
+        logsVisualElement = terminalInstance.Q("logs");
+        bodyVisualElement = terminalInstance.Q("body");
+        terminalWindow = terminalInstance.Q("window");
 
         rootVisualElement.Q<Button>("maximize-btn").RegisterCallback<ClickEvent>(Maximize);
         rootVisualElement.Q<Button>("minimize-btn").RegisterCallback<ClickEvent>(Minimize);
-        testGoals = new List<string>{"measure whatever in whatever and so whatever","measure whatever in whatever","measure whatever in whatever"};
 
+        terminalWindow.RegisterCallback<MouseDownEvent>(ev => { mouseDown = true; dragStartPos = ev.mousePosition; });
+        terminalWindow.RegisterCallback<MouseUpEvent>(ev => mouseDown = false);
+        terminalWindow.RegisterCallback<MouseMoveEvent>(HandleMouseDrag);
+        terminalWindow.RegisterCallback<MouseLeaveEvent>(ev => mouseDown = false);
+        terminalWindow.RegisterCallback<MouseEnterEvent>(ev => Debug.Log($"top:{terminalWindow.resolvedStyle.top} left:{terminalWindow.resolvedStyle.left}"));
         DrawGoals();
     }
 
-    private void Update() {
-        if(drawLogs){
+    private void Update()
+    {
+        #region for testing
+        if (drawLogs)
+        {
             DrawLogs();
             drawLogs = false;
         }
+        #endregion
     }
-
+    /// <summary>Writes logs to the terminal window</summary>
     public void Write(string log)
     {
         logs.Add(log);
         DrawLogs();
 
     }
-
+    #region maximize minimize
     private void Maximize(ClickEvent ev)
     {
         if (windowState != WindowState.Maximized)
         {
-            Debug.Log("Maximize clicked!");
             terminalWindow.style.height = Length.Percent(40);
 
             windowState = WindowState.Maximized;
         }
         else
         {
-            Debug.Log("Normal clicked!");
             terminalWindow.style.height = Length.Percent(30);
             // terminalWindow.style.width = Length.Percent(30);
             windowState = WindowState.Normal;
@@ -82,37 +96,55 @@ public class UiTerminal : MonoBehaviour
 
     private void Minimize(ClickEvent ev)
     {
-        Debug.Log("minimize clicked!");
-        
         bodyVisualElement.style.display = DisplayStyle.None;
         windowState = WindowState.Minimized;
-
     }
+    #endregion
 
+    /// <summary>
+    ///Draws the Logs list to the terminal window
+    ///depending on its current state
+    ///</summary>
     private void DrawLogs()
     {
-        if(windowState == WindowState.Normal){
+        if (windowState == WindowState.Normal)
+        {
             logsVisualElement.Clear();
             Debug.Log(logs.Count);
             logsVisualElement.Add(new Label($"<indent=30em>-{logs.LastOrDefault()}</indent>"));
 
         }
-        if(windowState == WindowState.Maximized){
+        if (windowState == WindowState.Maximized)
+        {
             logsVisualElement.Clear();
-            foreach(var log in logs){
-                logsVisualElement.Add(new Label($"<indent=30em>-{log}</indent>"));
+            foreach (var log in logs)
+            {
+                logsVisualElement.Add(new Label($"<indent=30em>-{RichText.Italic<RichText>(RichText.Color<string>(log, PaletteColor.Red))}</indent>"));
             }
         }
-        if(windowState == WindowState.Normal){}
     }
 
-    private void DrawGoals(){
-        if(windowState != WindowState.Minimized){
+    private void DrawGoals()
+    {
+        if (windowState != WindowState.Minimized)
+        {
             goalsVisualElement.Clear();
-            foreach(var goal in testGoals){
+            foreach (var goal in testGoals)
+            {
                 goalsVisualElement.Add(new Label($"<indent=30em><b><s>-{goal}</s></b></indent>"));
             }
         }
     }
 
+    private void HandleMouseDrag(MouseMoveEvent ev)
+    {
+        if (mouseDown)
+        {
+            terminalWindow.style.top = terminalWindow.resolvedStyle.top + (ev.mousePosition.y - dragStartPos.y);
+            terminalWindow.style.left = terminalWindow.resolvedStyle.left + (ev.mousePosition.x - dragStartPos.x);
+            dragStartPos = ev.mousePosition;
+        }
+    }
+
 }
+
