68d12a4da2ea65936fcb85db70926ecf80bdb00e
nourgaser <nourgaser2012@gmail.com>
2023-02-05 18:11:38 +0200

    game modes UI

--- Assets/UI/cs/UIGameModes.cs
+++ Assets/UI/cs/UIGameModes.cs
@@ -6,13 +6,16 @@ using System.Linq;
 
 public class UIGameModes : MonoBehaviour
 {
-    VisualElement document, zoomIn, zoomOut;
+    VisualElement document, normal, zoomIn, zoomOut, pan, wire;
 
     protected void Awake() {
         document = GameObject.Find("UIDocument").GetComponent<UIDocument>().rootVisualElement;
 
         zoomIn = document.Q("zoom-in-btn");
         zoomOut = document.Q("zoom-out-btn");
+        pan = document.Q("pan-btn");
+        normal = document.Q("normal-btn");
+        wire = document.Q("wiring-btn");
 
         Debug.Log(zoomIn);
     }
@@ -21,11 +24,18 @@ public class UIGameModes : MonoBehaviour
         OnDisable();
         zoomIn.RegisterCallback<MouseDownEvent>(HandleZoomIn);
         zoomOut.RegisterCallback<MouseDownEvent>(HandleZoomOut);
+        pan.RegisterCallback<MouseDownEvent>(HandlePan);
+        normal.RegisterCallback<MouseDownEvent>(HandleNormal);
+        wire.RegisterCallback<MouseDownEvent>(HandleWire);
     }
 
     private void OnDisable() {
         zoomIn.UnregisterCallback<MouseDownEvent>(HandleZoomIn);
         zoomOut.UnregisterCallback<MouseDownEvent>(HandleZoomOut);
+        pan.UnregisterCallback<MouseDownEvent>(HandlePan);
+        normal.UnregisterCallback<MouseDownEvent>(HandleNormal);
+        wire.UnregisterCallback<MouseDownEvent>(HandleWire);
+
     }
 
     private void HandleZoomIn(MouseDownEvent ev) {
@@ -36,5 +46,17 @@ public class UIGameModes : MonoBehaviour
     private void HandleZoomOut(MouseDownEvent ev) {
         Debug.Log("Unzooming");
         Camera.Zoom(-0.5f);
+    }
+
+    private void HandlePan(MouseDownEvent ev) {
+        GameManagement.GameMode.Pan();
+    }
+
+    private void HandleNormal(MouseDownEvent ev) {
+        GameManagement.GameMode.Normal();
+    }
+
+    private void HandleWire(MouseDownEvent ev) {
+        GameManagement.GameMode.Wire();
     }
 }
