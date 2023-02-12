8c280fc903bf9238ba8d6558e183627ae680ad08
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-02-12 21:32:05 +0200

    Fixed bug in issue #34

--- Assets/UI/cs/UIGameModes.cs
+++ Assets/UI/cs/UIGameModes.cs
@@ -20,41 +20,41 @@ public class UIGameModes : MonoBehaviour
 
     private void OnEnable() {
         OnDisable();
-        zoomIn.RegisterCallback<MouseDownEvent>(HandleZoomIn);
-        zoomOut.RegisterCallback<MouseDownEvent>(HandleZoomOut);
-        pan.RegisterCallback<MouseDownEvent>(HandlePan);
-        normal.RegisterCallback<MouseDownEvent>(HandleNormal);
-        wire.RegisterCallback<MouseDownEvent>(HandleWire);
+        zoomIn.RegisterCallback<ClickEvent>(HandleZoomIn);
+        zoomOut.RegisterCallback<ClickEvent>(HandleZoomOut);
+        pan.RegisterCallback<ClickEvent>(HandlePan);
+        normal.RegisterCallback<ClickEvent>(HandleNormal);
+        wire.RegisterCallback<ClickEvent>(HandleWire);
     }
 
     private void OnDisable() {
-        zoomIn.UnregisterCallback<MouseDownEvent>(HandleZoomIn);
-        zoomOut.UnregisterCallback<MouseDownEvent>(HandleZoomOut);
-        pan.UnregisterCallback<MouseDownEvent>(HandlePan);
-        normal.UnregisterCallback<MouseDownEvent>(HandleNormal);
-        wire.UnregisterCallback<MouseDownEvent>(HandleWire);
+        zoomIn.UnregisterCallback<ClickEvent>(HandleZoomIn);
+        zoomOut.UnregisterCallback<ClickEvent>(HandleZoomOut);
+        pan.UnregisterCallback<ClickEvent>(HandlePan);
+        normal.UnregisterCallback<ClickEvent>(HandleNormal);
+        wire.UnregisterCallback<ClickEvent>(HandleWire);
 
     }
 
-    private void HandleZoomIn(MouseDownEvent ev) {
+    private void HandleZoomIn(ClickEvent ev) {
         Debug.Log("Zooming");
         Camera.Zoom(0.5f);
     }
 
-    private void HandleZoomOut(MouseDownEvent ev) {
+    private void HandleZoomOut(ClickEvent ev) {
         Debug.Log("Unzooming");
         Camera.Zoom(-0.5f);
     }
 
-    private void HandlePan(MouseDownEvent ev) {
+    private void HandlePan(ClickEvent ev) {
         GameManagement.GameMode.Pan();
     }
 
-    private void HandleNormal(MouseDownEvent ev) {
+    private void HandleNormal(ClickEvent ev) {
         GameManagement.GameMode.Normal();
     }
 
-    private void HandleWire(MouseDownEvent ev) {
+    private void HandleWire(ClickEvent ev) {
         GameManagement.GameMode.Wire();
     }
 }
