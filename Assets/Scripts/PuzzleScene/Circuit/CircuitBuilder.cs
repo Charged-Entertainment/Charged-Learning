6f34f106d69ad10059f51147c3aec692e3ed1f0c
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-02-15 01:25:47 +0200

    inital Simulation system

--- Assets/Scripts/PuzzleScene/Circuit/CircuitBuilder.cs
+++ Assets/Scripts/PuzzleScene/Circuit/CircuitBuilder.cs
@@ -9,7 +9,7 @@ public class CircuitBuilder : Singleton<CircuitBuilder>
 {
     // Start is called before the first frame update
     private Dictionary<Terminal, Wire> terminalToWire;
-
+    private static Wire groundWire;
     protected new void Awake()
     {
         base.Awake();
@@ -33,7 +33,8 @@ public class CircuitBuilder : Singleton<CircuitBuilder>
 
     void HandleWireCreated(Wire wire)
     {
-        if (terminalToWire.ContainsKey(wire.t1) && terminalToWire.ContainsKey(wire.t2)){
+        if (terminalToWire.ContainsKey(wire.t1) && terminalToWire.ContainsKey(wire.t2))
+        {
             Debug.Log("Both terminals are already in the dict");
             return;
         }
@@ -58,5 +59,61 @@ public class CircuitBuilder : Singleton<CircuitBuilder>
     }
 
 
-    public void Collect(){}
+    public static SpiceSharp.Circuit Collect() {
+        var liveComponents = new HashSet<LiveComponent>();
+        var terminalToWire = Instance.terminalToWire;
+        foreach(var terminal in terminalToWire){
+            liveComponents.Add(terminal.Key.parent);
+        }
+
+        var circuit = new SpiceSharp.Circuit();
+
+        foreach(var liveComponent in liveComponents){
+            var entity = SpiceComponentFactory(liveComponent);
+            circuit.Add(entity);
+            Debug.Log($"circuit entity: {entity}");
+        }
+        return circuit;
+    }
+
+
+    /// <summary>Bad hack because Spice simulators require a ground node "0"</summary>
+    private static string GetWireName(Wire wire){
+        if(groundWire == null){
+            groundWire = wire;
+            return "0";
+        }else if(wire == groundWire)
+            return "0";
+        else
+            return wire.GetInstanceID().ToString();
+    }
+
+    private static SpiceSharp.Components.Component SpiceComponentFactory(LiveComponent liveComponent)
+    {
+        
+        var component = liveComponent.levelComponent.Component;
+        var terminalToWire = Instance.terminalToWire;
+
+        Debug.Log($"Component Type: {component.componentType}");
+        switch (component.componentType)
+        {
+            case ComponentType.Resistor:
+                
+                return new SpiceSharp.Components.Resistor(
+                    liveComponent.levelComponent.Name,
+                    GetWireName(terminalToWire[liveComponent.Terminals[0]]),
+                    GetWireName(terminalToWire[liveComponent.Terminals[1]]),
+                    component.Properties[PropertyType.Resistance].value
+                    );
+                
+            case ComponentType.Battery:
+                return new SpiceSharp.Components.VoltageSource(
+                    liveComponent.levelComponent.Name,
+                    GetWireName(terminalToWire[liveComponent.Terminals[0]]),
+                    GetWireName(terminalToWire[liveComponent.Terminals[1]]),
+                    component.Properties[PropertyType.Voltage].value
+                    );
+        }
+        return null;
+    }
 }
---
+++ Assets/Scripts/PuzzleScene/Simulation/SimulationManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using SpiceSharp.Validation;
+
+public class SimulationManager : Singleton<SimulationManager>
+{
+    // Start is called before the first frame update
+    [SerializeField] bool test = false;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (test)
+        {
+            CircuitBuilder.Collect();
+            test = false;
+        }
+    }
+
+    public static void Simulate()
+    {
+        //One node must be called "0" --represent the ground--
+        Debug.Log("Starting simulation");
+        var op = new SpiceSharp.Simulations.Transient("sim1");
+        var circuit = CircuitBuilder.Collect();
+        op.ExportSimulationData += HandleSimulationResults;
+
+        try
+        {            
+            op.Run(circuit);
+        }
+        catch (SpiceSharp.Simulations.ValidationFailedException e)
+        {
+            Debug.Log("exception");
+            FeebackTerminal.Write(new Log(e.Message, LogType.Error));
+            var violations = circuit.Validate();
+            foreach(var violation in violations.Violations){
+                Debug.Log(violation.Rule);
+                Debug.Log(violation.Subject);
+            }
+        }
+
+        op.AfterExecute += (sender, args) => {Debug.Log("Executed");};
+    }
+
+    private static void HandleSimulationResults(object sender, SpiceSharp.Simulations.ExportDataEventArgs args){
+        FeebackTerminal.Write(new Log($"Voltage on the ground '0' node {RichText.Color(args.GetVoltage("0").ToString(),PaletteColor.Green)}"));
+    }
+}
