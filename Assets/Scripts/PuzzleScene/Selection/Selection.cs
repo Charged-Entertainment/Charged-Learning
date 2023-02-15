e1bfef64e2b3322e36832a4af3b24cc9c98327b8
nourgaser <nourgaser2012@gmail.com>
2023-02-15 11:37:47 +0200

    Selection public method List -> IEnumerable

--- Assets/Scripts/PuzzleScene/Selection/Selection.cs
+++ Assets/Scripts/PuzzleScene/Selection/Selection.cs
@@ -105,7 +105,7 @@ public partial class Selection : Singleton<Selection>
         GameObject.Destroy(component.gameObject.GetComponent<SelectedObjectOverlay>());
     }
 
-    static public void InvertComponents(List<EditorBehaviour> components)
+    static public void InvertComponents(IEnumerable<EditorBehaviour> components)
     {
         foreach (var component in components)
         {
@@ -113,7 +113,7 @@ public partial class Selection : Singleton<Selection>
         }
     }
 
-    static public void AddComponents(List<EditorBehaviour> components)
+    static public void AddComponents(IEnumerable<EditorBehaviour> components)
     {
         foreach (var component in components)
         {
@@ -121,7 +121,7 @@ public partial class Selection : Singleton<Selection>
         }
     }
 
-    static public void RemoveComponents(List<EditorBehaviour> components)
+    static public void RemoveComponents(IEnumerable<EditorBehaviour> components)
     {
         foreach (var component in components)
         {
@@ -131,7 +131,7 @@ public partial class Selection : Singleton<Selection>
 
     static public void Clear()
     {
-        RemoveComponents(new List<EditorBehaviour>(selectedGameObjects.Values));
+        RemoveComponents(selectedGameObjects.Values);
         selectedGameObjects.Clear(); // just to make sure
     }
 
