1b0dc455d271c95335d9eb1790a35fb62dd2725c
nourgaser <nourgaser2012@gmail.com>
2023-02-20 11:48:40 +0200

    fixed knob broken from merge conflict

--- Assets/Scripts/PuzzleScene/Devices/Multimeter/Knob.cs
+++ Assets/Scripts/PuzzleScene/Devices/Multimeter/Knob.cs
@@ -18,16 +18,20 @@ public class Knob : MonoBehaviour
     ///<summary>Calculates the angle that the knob should be rotated at 
     ///when the mouse is dragging it.
     ///</summary>
-    private void OnMouseDrag() {
-        var diff = Utils.GetMouseWorldPosition() - transform.position;
-        diff.Normalize();
-        float rotationZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        var newAngle = Mathf.RoundToInt(rotationZ/degreePerMode)*degreePerMode;
-        newAngle = Mathf.Clamp(newAngle, minAngle, maxAngle);
-        CheckAngleChange(newAngle);
-        if(newAngle < 0)newAngle+=360;
-        currentRotationAngle = newAngle;
-        transform.localRotation = Quaternion.Euler(0,0, currentRotationAngle);        
+    private void OnMouseDrag()
+    {
+        if (isActiveAndEnabled)
+        {
+            var diff = Utils.GetMouseWorldPosition() - transform.position;
+            diff.Normalize();
+            float rotationZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+            var newAngle = Mathf.RoundToInt(rotationZ / degreePerMode) * degreePerMode;
+            newAngle = Mathf.Clamp(newAngle, minAngle, maxAngle);
+            CheckAngleChange(newAngle);
+            if (newAngle < 0) newAngle += 360;
+            currentRotationAngle = newAngle;
+            transform.localRotation = Quaternion.Euler(0, 0, currentRotationAngle);
+        }
     }
 
     private void CheckAngleChange(int newAngle)
@@ -37,18 +41,26 @@ public class Knob : MonoBehaviour
     }
 
     ///<summary>Takes an angle and return the appropriate mode for it.</summary>
-    private DeviceMode GetAngleMode(int angle){
-        if(angle==60)
+    private DeviceMode GetAngleMode(int angle)
+    {
+        if (angle == 60)
             return new ResistanceMode("200");
-        if(angle is 30)
+        if (angle is 30)
             return new VoltageMode(false, "200");
-        if(angle is 0)
+        if (angle is 0)
             return new OffMode();
-        if(angle is -30)
+        if (angle is -30)
             return new CurrentMode(false, "200");
         // if(angle is > -60); not implemented yet
-        
+
         // else
         return new LockedMode();
+    }
+
+    public static void SetEnabled(bool enabled)
+    {
+        var knob = GameObject.Find("multimeter-knob");
+        if (knob != null) knob.GetComponent<Knob>().enabled = enabled;
+        else Debug.Log("multimeter-knob doesn't exist");
     }
 }
