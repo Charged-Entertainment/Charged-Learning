0fb542d6bcd2bc20e3394291f3a378022758e332
nourgaser <nourgaser2012@gmail.com>
2023-03-01 15:55:49 +0200

    Added light to the LED (#63)

---
+++ Assets/Scripts/ComponentBehviours/Led.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class Led : MonoBehaviour
+{
+    public readonly static float MIN_INTENSITY = 0;
+    public readonly static float SAFE_MAX_INTENSITY = 10;
+    public readonly static float DANGEROUS_MAX_INTENSITY = 15;
+
+    [SerializeField] private new Light2D light;
+    [SerializeField] private float lerpSpeed = 3;
+    [SerializeField] private float targetIntensity = 0;
+    void Start()
+    {
+        light = gameObject.GetComponent<Light2D>();
+        light.intensity = 0;
+    }
+    private void Update()
+    {
+        if (targetIntensity != light.intensity)
+        {
+            light.intensity = Mathf.Lerp(light.intensity, targetIntensity, Time.deltaTime * lerpSpeed);
+        }
+    }
+    public void SetIntensity(float intensity)
+    {
+        targetIntensity = Mathf.Clamp(intensity, MIN_INTENSITY, DANGEROUS_MAX_INTENSITY);
+    }
+}
