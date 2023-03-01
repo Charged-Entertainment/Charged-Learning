5e37f57578f8660324637bed57eaa6b0e24337a6
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-03-01 03:53:45 +0200

    added '√' to supported symbols

--- Assets/Scripts/PuzzleScene/Devices/Calculator/Calculator.cs
+++ Assets/Scripts/PuzzleScene/Devices/Calculator/Calculator.cs
@@ -20,7 +20,7 @@ public class Calculator : EditorBehaviour
         {'?'},
 
         // Operstors
-        {'+'}, {'-'}, {'*'}, {'/'}, {'^'}, {'s'},
+        {'+'}, {'-'}, {'*'}, {'/'}, {'^'}, {'s'},{'√'},
 
         // Space
         {' '},
