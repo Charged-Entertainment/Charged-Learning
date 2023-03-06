01b54651a18c8e58b9ba1b9d02f527b93e5e8ba3
nourgaser <nourgaser2012@gmail.com>
2023-03-06 09:49:53 +0200

    LED current non-linear mapping to intensity value

--- Assets/Scripts/ComponentBehviours/Led/LedCurrentBehvior.cs
+++ Assets/Scripts/ComponentBehviours/Led/LedCurrentBehvior.cs
@@ -5,21 +5,32 @@ using GameManagement;
 
 public class LedCurrentBehvior : MonoBehaviour
 {
+    private readonly static float CURRENT_TO_INTENSITY_MULT = 0.0375f;
+    
+    /// <summary>
+    /// Non-linear (quadratic) conversion from current in milliamps to LED intensity.
+    /// </summary>
+    public static float CurrentToIntensity(float current_mA)
+    {
+        return Mathf.Pow(current_mA, 2) * CURRENT_TO_INTENSITY_MULT;
+    }
+
     Led led;
-    private void Awake() {
+    private void Awake()
+    {
         led = gameObject.GetComponent<Led>();
     }
 
     private void OnEnable()
     {
         SimulationManager.simulationDone += HandleCurrentChanged;
-        GameMode.changed += HandleGameModeChanged;  
+        GameMode.changed += HandleGameModeChanged;
     }
 
     private void OnDisable()
     {
         SimulationManager.simulationDone -= HandleCurrentChanged;
-        GameMode.changed -= HandleGameModeChanged;  
+        GameMode.changed -= HandleGameModeChanged;
 
     }
 
@@ -27,11 +38,15 @@ public class LedCurrentBehvior : MonoBehaviour
     {
         var id = gameObject.GetInstanceID().ToString();
         var current = new SpiceSharp.Simulations.RealPropertyExport(sim, id, "i").Value;
-        led.SetIntensity((float)(current * 1000));
+        led.SetIntensity(CurrentToIntensity((float)(current * 1000)));
+
+        // raise errors/warnings (icon/animation near LED + feedback terminal notification) if current is above the maximum rating or the recommended rating
     }
 
-    public void HandleGameModeChanged(State newState){
-        if(newState is Edit){
+    public void HandleGameModeChanged(State newState)
+    {
+        if (newState is Edit)
+        {
             led.SetIntensity(0);
         }
     }
