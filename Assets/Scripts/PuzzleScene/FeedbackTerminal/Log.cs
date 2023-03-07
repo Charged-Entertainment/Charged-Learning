8e80b08fb68a59b17dd5d53b0a504aa17d3c2e74
nourgaser <nourgaser2012@gmail.com>
2023-03-07 16:55:29 +0200

    Added notifications to terminal icon (#64)

--- Assets/Scripts/PuzzleScene/FeedbackTerminal/Log.cs
+++ Assets/Scripts/PuzzleScene/FeedbackTerminal/Log.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum LogType{Error, Status}
+public enum LogType{Error, Status, Warning}
 
 public class Log
 {
--- Assets/UI/cs/Tools/UITerminalIcon.cs
+++ Assets/UI/cs/Tools/UITerminalIcon.cs
@@ -8,10 +8,12 @@ public partial class UI
     public static Button TerminalButton { get; private set; } = null;
     private class UITerminalIcon : UIBaseElement
     {
+        private static VisualElement Notification;
         private void Awake()
         {
             // var terminal = rootVisualElement.Q("terminal-instance");
             TerminalButton = document.Q<Button>("terminal-btn");
+            Notification = document.Q("terminal-btn-notification");
             FeebackTerminal.disabled += () =>
             {
                 TerminalButton.SetEnabled(true);
@@ -20,12 +22,45 @@ public partial class UI
             FeebackTerminal.enabled += () =>
             {
                 TerminalButton.SetEnabled(false);
+                HideNotification();
             };
             TerminalButton.RegisterCallback<ClickEvent>(e =>
             {
                 FeebackTerminal.Enable();
             });
+
+            Notification.visible = false;
             TerminalButton.SetEnabled(false);
+        }
+
+        private void OnEnable()
+        {
+            FeebackTerminal.LogWritten += HandleLogWritten;
+        }
+
+        private void OnDisable()
+        {
+            FeebackTerminal.LogWritten -= HandleLogWritten;
+        }
+
+        void ShowNotification(Color color)
+        {
+            Notification.visible = true;
+            Notification.style.backgroundColor = color;
+        }
+
+        void HideNotification()
+        {
+            Notification.visible = false;
+            Notification.style.backgroundColor = Color.black;
+            Notification.style.visibility = Visibility.Hidden;
+        }
+
+        void HandleLogWritten(LogType type)
+        {
+            if (type is LogType.Error) ShowNotification(Color.red);
+            else if (type is LogType.Warning && Notification.style.backgroundColor != Color.red) ShowNotification(Color.yellow);
+            else if (type is LogType.Status && Notification.style.backgroundColor != Color.red && Notification.style.backgroundColor != Color.yellow) ShowNotification(Color.white);
         }
     }
 }
