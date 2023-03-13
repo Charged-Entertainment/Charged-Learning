4ae6c98503d8a6444795c8f555286237345c8fd7
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-03-14 01:50:07 +0200

    inital version

--- Assets/Scripts/PuzzleScene/GameManagement/GameManager.cs
+++ Assets/Scripts/PuzzleScene/GameManagement/GameManager.cs
@@ -16,21 +16,16 @@ public partial class GameManager : Singleton<GameManager>
     private void OnEnable() {
         OnDisable();
         GameMode.changed += SetToNormalInteraction;
-        GameMode.changed += Simulate;
     }
 
     private void OnDisable() {
         GameMode.changed -= SetToNormalInteraction;
-        GameMode.changed -= Simulate;
     }
 
     private static void SetToNormalInteraction(GameModes mode) {
         GameMode.Normal();
     }
 
-    private static void Simulate(GameModes mode = null) {
-        if (mode == GameModes.Evaluate || mode == GameModes.Live) SimulationManager.Simulate();
-    }
 
     static public void SetContollerEnabled(bool enabled) {
         Instance.gameController.enabled = enabled;
