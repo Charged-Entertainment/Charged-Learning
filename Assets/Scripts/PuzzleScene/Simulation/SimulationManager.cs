33145be349b670a158e0bf57969da5ef92e292ff
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-03-14 01:50:07 +0200

    inital version

--- Assets/Scripts/PuzzleScene/Simulation/SimulationManager.cs
+++ Assets/Scripts/PuzzleScene/Simulation/SimulationManager.cs
@@ -4,27 +4,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using SpiceSharp.Validation;
+using SpiceSharp.Simulations.IntegrationMethods;
 using Components;
+using GameManagement;
 
 public class SimulationManager : Singleton<SimulationManager>
 {
     private static SpiceSharp.Simulations.IBiasingSimulation simulation;
     public static Action<SpiceSharp.Simulations.IBiasingSimulation> simulationDone;
-    void Update()
-    {
+    private static double startTime = 0;
+    private static bool ongoingSimulation = false;
+    private static double elapsed = 0;
 
+    private new void Awake()
+    {
+        base.Awake();
+        GameMode.changed += HandleGameModeChanged;
     }
 
-    public static void Simulate()
+    void Update()
+    {
+        if (ongoingSimulation)
+        {
+            elapsed += Time.deltaTime;
+            if (elapsed >= 0.1f)
+            {
+                elapsed = elapsed % 0.1f;
+                startTime++;
+                Simulate();
+            }
+        }
+    }
+
+    private static void Simulate()
     {
         //One node must be called "0" --represent the ground--
         Debug.Log("Starting simulation");
-        simulation = new SpiceSharp.Simulations.Transient("sim1");
+        var timeParameters = new FixedEuler() { StartTime = startTime, StopTime = startTime, Step = 1 };
+        simulation = new SpiceSharp.Simulations.Transient("sim1", timeParameters);
 
         var circuit = CircuitBuilder.Collect();
 
         // TEMP for testing. Get attach real models!!
-        var testModel = new SpiceSharp.Components.DiodeModel("led_model"); 
+        var testModel = new SpiceSharp.Components.DiodeModel("led_model");
         // testModel.SetParameter("Rs", 330.0);
         circuit.Add(testModel);
 
@@ -44,6 +66,18 @@ public class SimulationManager : Singleton<SimulationManager>
                 Debug.Log(violation.Subject);
             }
         }
+    }
+    void HandleGameModeChanged(State state)
+    {
+        if (state == GameModes.Edit)
+        {
+            ongoingSimulation = false;
+            startTime = 0;
+            return;
+        }
+
+        ongoingSimulation = true;
+
     }
 
     private static void HandleSimulationResults(object sender, SpiceSharp.Simulations.ExportDataEventArgs args)
