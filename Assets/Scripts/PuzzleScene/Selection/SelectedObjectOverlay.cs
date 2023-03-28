e731fc132cd7b79d912f7540003140bbacb0ec2e
nourgaser <nourgaser2012@gmail.com>
2023-03-28 09:12:37 +0200

    fix selection overlay rotation

--- Assets/Scripts/PuzzleScene/Selection/SelectedObjectOverlay.cs
+++ Assets/Scripts/PuzzleScene/Selection/SelectedObjectOverlay.cs
@@ -8,8 +8,11 @@ public class SelectedObjectOverlay : MonoBehaviour
     GameObject go;
     private void Awake() {
         var prefab = Resources.Load<GameObject>("Prefabs/SelectedObjectOverlay");
-        go = GameObject.Instantiate(prefab, transform);
+        go = GameObject.Instantiate(prefab, transform);        
+        var oldRotation = transform.eulerAngles;
+        transform.eulerAngles = Vector3.zero;
         go.transform.localScale = transform.GetComponent<SpriteRenderer>().bounds.extents*2;
+        transform.eulerAngles = oldRotation;
     }
 
     private void OnDestroy() {
